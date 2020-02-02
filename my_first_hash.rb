ee2fb545f6e2af4c363d27ea80ac26dfba626a78
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 01:56:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,10 +20,10 @@ def retrieval
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-shipping_manifest["oil paintings"]
+
 end
 
-
+shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
@@ -41,4 +41,10 @@ def adding
 
   # return the shipping_manifest hash below
 
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
+shipping_manifest
 end
+
+
+shipping
